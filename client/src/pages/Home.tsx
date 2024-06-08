6b64f42ee5e3ec364eec06d3335f6a37078eb8e7
Steven He <hess1@uci.edu>
2024-06-08 01:15:44 -0700

    Move submodule + basic article demo

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>intuision</h1>
+      <p>"length" -lebron</p>
+    </div>
+  );
+};
+
+export default Home;
--- server/index.ts
+++ server/index.ts
@@ -10,33 +10,33 @@ import User, { IUser } from "./models/User";
 const mongoURI = process.env.MONGO_CONNECTION_URI || "";
 
 // MongoDB Connection
-mongoose
-  .connect(mongoURI, {})
-  .then(() => {
-    console.log("MongoDB Connected");
+// mongoose
+//   .connect(mongoURI, {})
+//   .then(() => {
+//     console.log("MongoDB Connected");
 
-    //test adding user
-    /*
-    async function createUser() {
-      const user: IUser = new User({
-        name: "John Doe",
-        email: "john.doe@example.com",
-        age: 30,
-      });
+//     //test adding user
+//     /*
+//     async function createUser() {
+//       const user: IUser = new User({
+//         name: "John Doe",
+//         email: "john.doe@example.com",
+//         age: 30,
+//       });
 
-      try {
-        const savedUser = await user.save();
-        console.log("User saved:", savedUser);
-      } catch (error) {
-        console.error("Error saving user:", error);
-      }
-    }
+//       try {
+//         const savedUser = await user.save();
+//         console.log("User saved:", savedUser);
+//       } catch (error) {
+//         console.error("Error saving user:", error);
+//       }
+//     }
 
-    createUser(); */
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+//     createUser(); */
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
 // Express Server
 
