b9e489b734a3f2527c021271f39e53b8ccae39b4
Evan Kim <evan.e.kim@gmail.com>
2024-06-19 22:25:12 +0900

    styled accordion files which render from a json

--- client/src/components/articles/ControlPanel.tsx
+++ client/src/components/articles/ControlPanel.tsx
@@ -5,105 +5,105 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { Button, Flex, Box } from "@radix-ui/themes";
+import { BookmarkFilledIcon, BookmarkIcon } from "@radix-ui/react-icons";
 
-const ControlPanel = () => {
+const ControlPanel = ({ switchTab }) => {
   //maybe change this to native radix scroll because style's slightly different
+  const test_toc = {
+    Kinematics: {
+      "1D": {
+        "Ch1.1.1": ["a", "b", "c"],
+      },
+      "2D": {
+        "Ch1.2.1": ["a", "b", "c"],
+      },
+    },
+    Dynamics: {
+      "Newton's First Law": {
+        "Ch2.1.1": ["a", "b", "c"],
+      },
+      "Newton's Second Law": {
+        "Ch2.2.1": ["a", "b", "c"],
+      },
+    },
+    Dynamics2: {
+      "Newton's First Law": {
+        "Ch2.1.1": ["a", "b", "c"],
+      },
+      "Newton's Second Law": {
+        "Ch2.2.1": ["a", "b", "c"],
+      },
+    },
+  };
+
+  const FileList = (files: string[], prefix: string, shift: number) => {
+    return (
+      <Flex direction="column" justify="start" align="start">
+        {files.map((title, index) => (
+          // <Button
+          //   className="w-full rounded-sm hover:bg-[#282828] pl-1"
+          //   variant="ghost"
+          // >
+          //   {`${prefix}${index + 1}. ${title}`}
+          // </Button>
+          <button
+            className={`w-full rounded-sm hover:bg-[#282828] hover:cursor-pointer pl-${shift} text-left`}
+            onClick={() => {
+              console.log("test");
+            }}
+          >
+            <Flex align="center">
+              <BookmarkIcon className="mx-1" />
+              {`${prefix}${index + 1}. ${title}`}
+            </Flex>
+          </button>
+        ))}
+      </Flex>
+    );
+  };
+
+  //ULTRA SCUFFED LOL
+  const RecursiveFileSystem = (
+    files: Object,
+    prefix: string,
+    shift: number,
+  ) => {
+    console.log(shift);
+    return (
+      <Accordion type="multiple">
+        {Object.keys(files).map((title, index) => (
+          <AccordionItem value={`Chapter ${prefix}${index + 1}.`}>
+            <AccordionTrigger
+              //shifting it to give the file look
+              className={`pl-${shift}`}
+            >{`${prefix}${index + 1}. ${title}`}</AccordionTrigger>
+            <AccordionContent>
+              {!files[title][0]
+                ? RecursiveFileSystem(
+                    files[title],
+                    `${prefix}${index + 1}.`,
+                    shift + 3,
+                  )
+                : FileList(files[title], `${prefix}${index + 1}.`, shift + 3)}
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    );
+  };
+
   return (
-    <ScrollArea className="border-b w-full h-full pr-3 p-2">
-      <Accordion
-        type="single"
-        collapsible
-        className="hover:bg-[#555555] rounded-sm"
-      >
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1" className="px-4 border-0">
-          <AccordionTrigger>Section</AccordionTrigger>
-          <AccordionContent>Subsection</AccordionContent>
-        </AccordionItem>
-      </Accordion>
+    <ScrollArea className="border-b w-full h-full">
+      <Box className="p-4">{RecursiveFileSystem(test_toc, "", 0)}</Box>
+      {/* <Accordion type="multiple" className="m-4">
+        {test_toc.map((title, index) => (
+          <AccordionItem value={`Chapter ${index + 1}`}>
+            <AccordionTrigger>{`${index + 1}. ${title}`}</AccordionTrigger>
+            <AccordionContent>testing</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion> */}
     </ScrollArea>
   );
 };
--- client/src/pages/Article.tsx
+++ client/src/pages/Article.tsx
@@ -4,11 +4,28 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import ArticleContent from "@/components/articles/ArticleContent";
-import { Flex, Tabs, Box, Heading } from "@radix-ui/themes";
+import { Flex, Box, Heading, Button } from "@radix-ui/themes";
+import { Tabs } from "@radix-ui/themes";
 import ControlPanel from "@/components/articles/ControlPanel";
 import NavBar from "@/components/NavBar";
+import { useRef, useState } from "react";
+import Scene from "@/components/articles/Scene";
 
 const Article = () => {
+  //  const [activeArticle, setActiveArticle] = useState(NULL);
+
+  //All this stuff here is for dealing with split screening
+  const [activeScene, setActiveScene] = useState(null);
+  const [splitVert, setSplitVert] = useState(false);
+  const [splitHorizontal, setSplitHorizontal] = useState(false);
+
+  const handleExpand = () => {
+    console.log(splitHorizontal);
+    setSplitHorizontal(true);
+    setSplitVert(true);
+    //if (splitScreen) setActive
+  };
+
   return (
     <Flex height="100vh" m="0" p="0" width="100%" direction="column">
       <NavBar />
@@ -27,20 +44,59 @@ const Article = () => {
         </ResizablePanel>
         <ResizableHandle withHandle={false} />
         <ResizablePanel>
-          <Box py="1">
-            <Tabs.Root defaultValue="tab1">
-              <Tabs.List>
-                <Tabs.Trigger value="tab1">Tab 1</Tabs.Trigger>
-                <Tabs.Trigger value="tab2">Tab 2</Tabs.Trigger>
-              </Tabs.List>
-              <Box width="100%" height="100%">
-                <Tabs.Content value="tab1">
-                  <ArticleContent />
-                </Tabs.Content>
-                <Tabs.Content value="tab2">asdfghjkl</Tabs.Content>
-              </Box>
-            </Tabs.Root>
-          </Box>
+          <ResizablePanelGroup direction="vertical">
+            <ResizablePanel id="top-split" order={1}>
+              <ResizablePanelGroup direction="horizontal">
+                <ResizablePanel id="top-left-split" order={1}>
+                  <Tabs.Root defaultValue="tab1">
+                    <Tabs.List>
+                      <Tabs.Trigger value="tab1">
+                        <div>Tab 1</div>
+                      </Tabs.Trigger>
+                      <Tabs.Trigger value="tab2">Tab 2</Tabs.Trigger>
+                    </Tabs.List>
+                    <Box width="100%" height="100%">
+                      <Tabs.Content value="tab1">
+                        <ArticleContent onExpand={handleExpand} />
+                      </Tabs.Content>
+                      <Tabs.Content value="tab2">
+                        <ArticleContent onExpand={handleExpand} />
+                      </Tabs.Content>
+                    </Box>
+                  </Tabs.Root>
+                </ResizablePanel>
+                {splitHorizontal && (
+                  <>
+                    <ResizableHandle withHandle={false} />
+                    <ResizablePanel
+                      id="top-right-split"
+                      order={2}
+                      defaultSize={50}
+                    >
+                      <ArticleContent />
+                    </ResizablePanel>
+                  </>
+                )}
+              </ResizablePanelGroup>
+            </ResizablePanel>
+            {splitVert && (
+              <>
+                <ResizableHandle withHandle={false} />
+                <ResizablePanel
+                  id="bottom-split"
+                  order={2}
+                  defaultSize={30}
+                  className="bg-[#181818]"
+                >
+                  <Scene
+                    caption="asdf"
+                    in_article={false}
+                    onExpand={handleExpand}
+                  />
+                </ResizablePanel>
+              </>
+            )}
+          </ResizablePanelGroup>
         </ResizablePanel>
       </ResizablePanelGroup>
     </Flex>
